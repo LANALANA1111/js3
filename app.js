57c0aadfdf062bbf45a6247dc489811c2c94c3ee
LANALANA1111 <lanagelashvili1111@gmail.com>
2024-01-14 01:18:00 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,116 @@
+// 1) დაწერეთ კოდი სადაც შეამოწმებთ დადებითია თუ უარყოფითი 
+// ეს რიცხვი!!
+
+// answer:
+
+// let number = Number(prompt("Type..."))
+// if (number > 0 ){
+//     console.log("დადებითია")
+// }else if (number < 0 ){
+//     console.log("უარყოფითია")
+// }
+
+
+// 2)დაწერეთ მარტივი კალკულატორი სადაც გამოიყენებთ ოპერატორებს(+, -, *, /)
+// switch case დახმარებით უბრალოდ დარწმუნდით რომ თითოეულ ქეისი ასრულებს ამ მეთოდებს 
+
+// asnwer: 
+
+
+// let num1 = Number(prompt());
+
+// switch (num1) {
+//       case num1:
+//         console.log(num1 + 1);
+//         break;
+//       default:
+//         console.log("Error");
+//     }
+
+
+
+// let num2 = Number(prompt());
+
+// switch (num1) {
+//       case num1:
+//         console.log(num1 - 1);
+//         break;
+//       default:
+//         console.log("Error");
+//     }
+
+
+
+
+// let num3 = Number(prompt());
+
+// switch (num1) {
+//       case num1:
+//         console.log(num1 * 1);
+//         break;
+//       default:
+//         console.log("Error");
+//     }
+
+
+
+//     let num4 = Number(prompt());
+
+// switch (num1) {
+//       case num1:
+//         console.log(num1 / 1);
+//         break;
+//       default:
+//         console.log("Error");
+//     }
+
+
+//3) დაწერეთ იგივე კალკულატორი if else დახმარებით!!
+
+//asnwer: 
+
+
+// let num1 = Number(prompt());
+
+// if (num1) {
+//     console.log(num1 + 1);
+// }else {
+//     console.log("error")
+// }
+
+
+// let num2 = Number(prompt());
+
+// if (num2) {
+//     console.log(num2 - 1);
+// }else { 
+//     console.log("error")
+// }
+
+
+// let num3 = Number(prompt());
+
+// if (num3){
+//     console.log(num3 * 1);
+// }else {
+//     console.log("error")
+// }
+
+
+// let num4 = Number(prompt());
+
+// if (num4) {
+//     console.log(num4 / 1);
+// }else {
+//     console.log("error")
+// }
+
+
+//4) დაწერეთ ავტორიზაციის ლოგიკა სადაც უნდა დავარეგისტრიროთ User
+// user უნდა შემოყავდეს firstName,lastName,email,password,phoneNumber
+// firstName,lastName,email,password არის სავალდებულო ველები
+// ამიტომ ერთიანად შეამოწმეთ ეს ოთხი ველი და დარწმუნდით თუ ცარიელი input 
+// არარის მაშინ დალოგეთ რომ შენ დარეგისტრირდი წარმატებით თუ ცარიელია მაშინ
+// დალოგეთ "input is required" გამოიყენეთ if else 
+
+//answer:
